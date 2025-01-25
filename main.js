ecba0f16172283835901251b1783a2d6fc7d3e01
grawash <67363329+grawash@users.noreply.github.com>
2025-01-25 17:55:53 +0400

    add route to fetch and send JSON data

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import {express} from "express"
+import express from "express";
+import fs from "fs/promises";
+import path from "path"
+
+const app = express();
+
+app.get('/', async (req,res) => {
+    const data = await fs.readFile('expenses.json', 'utf-8')
+    const expenses = await JSON.parse(data);
+    res.send(expenses)
+})
+
+app.listen(3000, () => {
+    console.log('server running on http://localhost:3000')
+})
