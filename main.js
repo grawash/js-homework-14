c23f945ed0c90a5e6d6b089d123262e0f76d03e7
grawash <67363329+grawash@users.noreply.github.com>
2025-01-25 18:37:12 +0400

    add DELETE route to delete expenses

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ app.post('/api', async (req,res) => {
     const expenses = await JSON.parse(data);
     const lastId = expenses[expenses.length -1]?.id || 0;
     const newExpense = {
-        id: lastId,
+        id: lastId + 1,
         category,
         price,
         date: new Date().toISOString(),
@@ -41,6 +41,20 @@ app.post('/api', async (req,res) => {
     res.json({"message": "new expense added successfuly", "data": newExpense})
 })
 
+app.delete('/api/:id', async (req, res) => {
+    const {id} = req.params
+    const data = await fs.readFile('expenses.json', 'utf-8')
+    const expenses = await JSON.parse(data);
+    const foundExpense = expenses.find(el => el.id === Number(id))
+    const index = expenses.indexOf(foundExpense)
+    if(!foundExpense){
+        return res.status(404).json({"message": "expense not found"})
+    }
+    expenses.splice(index, 1);
+    await fs.writeFile('expenses.json', JSON.stringify(expenses, null, 2));
+    res.json({"message":"expense deleted successfuly", "data": foundExpense})
+
+})
 app.listen(3000, () => {
     console.log('server running on http://localhost:3000')
 })
